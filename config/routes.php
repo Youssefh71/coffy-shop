b847198f8d19493a6a85815fa82d4d0224f3f3e0
Agnija <ilzena.agnija@gmail.com>
2022-05-18 16:04:57 +0200

    route- insert, not found

---
+++ config/routes.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once '../src/Controller/CoffeeController.php';
+require_once '../src/Controller/ErrorController.php';
+
+
+switch($uri) {
+    //Accueil
+    case '/': 
+       $controller = new CoffeeController();
+        // Charge la méthode correspondant à la vue souhaitée
+       $controller->insert();
+       break;
+
+    default:
+    //  echo '<h1>Erreur 404</h1>';
+        $controller = new ErrorController();
+        $controller->notFound();
+    }
