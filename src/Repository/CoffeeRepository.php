8ac64663a1711c37a33c288829c002a1ff442a91
Youssef Hourri <youssefhourri69@gmail.com>
2022-05-18 16:26:07 +0200

    CoffeRepository

---
+++ src/Repository/CoffeeRepository.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once 'Db.php';
+require_once __DIR__ .'../../Entity/Coffee.php';
+
+/**
+ * Gère le CRUD sur la table "avis"
+ */
+class CoffeRepository extends Db {
+
+    /**
+     * Insertion dans la table SQL "avis"
+     */
+    public function add(Entity\Coffee $coffee)
+    {
+        $query = $this->getDb()->prepare('INSERT INTO coffee (name, description, recette, image) VALUES (:name, :description, :recette, :image)');
+        $query->bindValue(':name', $coffee->getName());
+        $query->bindValue(':description', $coffee->getDescription());
+        $query->bindValue(':recette', $coffee->getRecette());
+        $query->bindValue(':image', $coffee->getImage());
+
+        return $query->execute();
+    }
+
+
+
+
+
+
+}
