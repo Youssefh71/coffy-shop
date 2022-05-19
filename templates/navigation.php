a0f6f08d111e6aa30c513b1a6eab31d647b612e2
Agnija <ilzena.agnija@gmail.com>
2022-05-19 14:32:08 +0200

    css nav

--- templates/navigation.php
+++ templates/navigation.php
@@ -1,22 +1,25 @@
 <div class="container">
-<nav class="navbar navbar-expand-lg bg-light">
+<nav class="navbar navbar-expand-lg ">
   <div class="container-fluid">
     <a class="navbar-brand" href="img/1.png">
     <img src="img/1.png"  alt="logo" class="img-logo">
     </a>
+    <div class="row d-flex justify-content-center">
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+   
+    <div class="collapse navbar-collapse " id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
         <li class="nav-item">
-          <a class="nav-link active menu" aria-current="page" href="#">Products</a>
+          <a class="nav-link  menu" aria-current="page" href="#">Products</a>
         </li>
         <li class="nav-item">
           <a class="nav-link menu" href="#">Ajouter</a>
         </li>
        </ul>
       
+    </div>
     </div>
   </div>
 </nav>
