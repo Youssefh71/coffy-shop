fbce62d4331417ed0ed88b2bbed7cfa07e8a9ba4
Youssef Hourri <youssefhourri69@gmail.com>
2022-05-19 16:56:15 +0200

    ok

--- templates/liste.php
+++ templates/liste.php
@@ -5,24 +5,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS -->
+  <link rel="stylesheet" href="../css/style.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <title>Coffee WF3 Shop</title>
 </head>
 
 <body>
-    <!DOCTYPE html>
-    <html lang="en">
-
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>liste café</title>
-        <!-- CSS -->
-  <link rel="stylesheet" href="../css/style.css">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    </head>
-
-    <body>
+    
     <?php require_once 'navigation.php' ?>
         <div class="w-75 mx-auto pt-5 ">
             
@@ -79,9 +69,7 @@
                 </tbody>
             </table>
         </div>
-    </body>
-
-    </html>
+    
 </body>
 
 </html>
