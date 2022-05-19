c477cea57e0b6863b770994a8c0fc4f820d3b88a
Youssef Hourri <youssefhourri69@gmail.com>
2022-05-19 12:20:19 +0200

    tt

--- templates/edit.php
+++ templates/edit.php
@@ -1,14 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
-<<<<<<< HEAD
-
-=======
->>>>>>> 2ba3cad7fd26875cbdf5f5c53b3dc648a4a78d98
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<<<<<<< HEAD
     <title>Poste ton avis !</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 </head>
@@ -70,18 +65,4 @@
     </div>
 </body>
 
-=======
-    <!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-
-    <title>Edition coffee</title>
-</head>
-<body>
- <div class="row">
-     
-     
- </div>
-    
-</body>
->>>>>>> 2ba3cad7fd26875cbdf5f5c53b3dc648a4a78d98
 </html>
